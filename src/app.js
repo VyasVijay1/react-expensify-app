a8d9ecf99893cfefcfee4c8b5b571faa8d5b0280
VyasVijay1 <learning.curve.42@gmail.com>
2019-12-31 13:11:52 -0700

    Fetching data from firebase

--- src/app.js
+++ src/app.js
@@ -2,50 +2,86 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/store'
+import {startSetExpenses} from './actions/expenses'
 import {addExpense} from './actions/expenses'
 import { setTextFilter } from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import {Provider} from 'react-redux'
 import 'react-dates/lib/css/_datepicker.css'
 import './styles/style.css' 
+import './firebase/firebase'
 
 const store = configureStore();
-// store.dispatch(addExpense({description: 'Water Bill', amount:4500, note: 'bill'}));
-// store.dispatch(addExpense({description: 'Gas Bill'}));
-//store.dispatch(setTextFilter('water'));
 
-// setTimeout (() => {
-//     store.dispatch(setTextFilter('bill'))
-// },3000)
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
 
-//const state = store.getState();
-// console.log(state);
-// const visibleExpense = getVisibleExpenses(state.expenses, state.filters)
-// console.log(visibleExpense)
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-const jsx =(
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
-)
-ReactDOM.render(jsx, document.getElementById('app'));
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
 
-// class OldSyntax {
-//     constructor(){
-//         this.name = 'Vijay'
-//     }
-//     getGreeting(){
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import AppRouter from './routers/AppRouter'
+// import configureStore from './store/store'
+// import {startSetExpenses} from './actions/expenses'
+// import {addExpense} from './actions/expenses'
+// import { setTextFilter } from './actions/filters'
+// import getVisibleExpenses from './selectors/expenses'
+// import {Provider} from 'react-redux'
+// import 'react-dates/lib/css/_datepicker.css'
+// import './styles/style.css' 
+// import './firebase/firebase'
+
+
+// const store = configureStore();
+// // store.dispatch(addExpense({description: 'Water Bill', amount:4500, note: 'bill'}));
+// // store.dispatch(addExpense({description: 'Gas Bill'}));
+// //store.dispatch(setTextFilter('water'));
+
+// // setTimeout (() => {
+// //     store.dispatch(setTextFilter('bill'))
+// // },3000)
+
+// //const state = store.getState();
+// // console.log(state);
+// // const visibleExpense = getVisibleExpenses(state.expenses, state.filters)
+// // console.log(visibleExpense)
+
+// const jsx =(
+//     <Provider store={store}>
+//         <AppRouter />
+//     </Provider>
+// )
+
+// ReactDOM.render(<p>Loding...</p>, document.getElementById('app'));
+// store.dispatch(startSetExpenses()).then(() => {
+//     ReactDOM.render(jsx, document.getElementById('app'));
+// })
+
+
+// // class OldSyntax {
+// //     constructor(){
+// //         this.name = 'Vijay'
+// //     }
+// //     getGreeting(){
         
-//     }
-// }
-// const oldSyntax = new OldSyntax();
-// class NewSyntax{
-//     name = 'Jen';
-//     getGreeting = () => {
-//         return <div>"Hi. My name is ${this.name}"</div>
-//     }
-// }
-// const newSyntax = new NewSyntax();
-// const newGreeting = newSyntax.getGreeting()
-// console.log(oldSyntax)
-// //console.log(newGreeting())
+// //     }
+// // }
+// // const oldSyntax = new OldSyntax();
+// // class NewSyntax{
+// //     name = 'Jen';
+// //     getGreeting = () => {
+// //         return <div>"Hi. My name is ${this.name}"</div>
+// //     }
+// // }
+// // const newSyntax = new NewSyntax();
+// // const newGreeting = newSyntax.getGreeting()
+// // console.log(oldSyntax)
+// // //console.log(newGreeting())
---
+++ src/firebase/firebase.js
@@ -0,0 +1,158 @@
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDe4HkLQs-AV1i7v3jRgeL3WQghTHANq5U",
+    authDomain: "expensify-59830.firebaseapp.com",
+    databaseURL: "https://expensify-59830.firebaseio.com",
+    projectId: "expensify-59830",
+    storageBucket: "expensify-59830.appspot.com",
+    messagingSenderId: "540449549979",
+    appId: "1:540449549979:web:2a1c25f7f17f37a96fe03e",
+    measurementId: "G-Q3841NTC2L"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+ 
+  const database =  firebase.database()
+ 
+export { firebase, database as default}
+
+  //refrense to Section 14 - firebase basics
+  
+//   database.ref('expenses').on('child_removed',(snapshot) => {
+//       console.log('id: ' + snapshot.key, snapshot.val())
+//   })
+
+//   database.ref('expenses').on('child_changed',(snapshot) => {
+//     console.log('id: ' + snapshot.key, snapshot.val())
+// })
+// database.ref('expenses').on('child_added',(snapshot) => {
+//     console.log('id: ' + snapshot.key, snapshot.val())
+// })
+ // database.ref('expenses/-LxNnujsoTjySlWr555F').remove()
+//   database.ref('expenses').push({
+//       description:'Rent',
+//     note: '1: This is notes 1',
+//     amount: 109500,
+//     createAt: 1000
+//   })
+//   database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//       const expenses = []
+//       snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         })
+//       })
+//       console.log(expenses)
+//   })
+//   database.ref('expenses').push({
+//     description:'Phone Bill',
+//     note: '2: this is notes 2',
+//     amount: 5900,
+//     createAt: 2000
+// })
+// database.ref('expenses')
+// .once('value')
+// .then((snapshot) => {
+//     const expenses = []
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//           id: childSnapshot.key,
+//           ...childSnapshot.val()
+//       })
+//     })
+//     console.log(expenses)
+// })
+// database.ref('expenses').push({
+//     description:'Food',
+//     note: '3: this is notes 3',
+//     amount: 1200,
+//     createAt: 3000
+// })
+// database.ref('expenses')
+// .once('value')
+// .then((snapshot) => {
+//     const expenses = []
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//           id: childSnapshot.key,
+//           ...childSnapshot.val()
+//       })
+//     })
+//     console.log(expenses)
+// })
+  //database.ref('notes/-LxNe-OtzHohat-le4fh').remove()
+//   database.ref('notes').push({
+//     title: 'Fitness',
+//     body: 'Running'
+// })
+
+//   database.ref().on('value',
+//   (snapshot) => {
+//       const val = snapshot.val()
+//       console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`)
+//   })
+//   database.ref('job/title').set('Vice President')
+//   setTimeout(() =>{
+//     database.ref('job').set('Vice President')
+//   }, 3500)
+  
+//   setTimeout(() =>{
+//     database.ref('age').set(48)
+//   }, 7000)
+  
+//   setTimeout(() =>{
+//     database.ref().off()
+//   }, 5000)
+
+//   setTimeout(() =>{
+//     database.ref('location/city').set('sausalito')
+//   }, 10500)
+//   database.ref('location/city')
+//     .once('value')
+//     .then((snapshot)=>
+//     {
+//         const val = snapshot.val()
+//         console.log(val)
+//     }).catch((e) => {
+//     console.log('This is exception: ' + e)
+//     })
+//   database.ref().set({
+//       name: 'Vijay Vyas',age: 30,
+//       job: 'Senior Software Developer',
+//       isSingle:false,
+//       location:{
+//           city: 'Phoenix',
+//           country: 'United States'
+//       }
+//   }).then(() => {
+//       console.log('Data is saved')
+//   }).catch((e) => {
+//       console.log('This failed', e)
+//   })
+
+//   database.ref().update({
+//       job: 'Manager',
+//       'location/city': 'Seattle'
+//   })
+//   database.ref().update({
+//     age: 30,
+//     'location/city': 'Seattle'
+// })
+//   database.ref('age').set(29)
+//   database.ref('location/city').set('New York')
+ 
+//   database.ref('attribute').set({
+//     height: 83,
+//     weight: 160
+//   })
+//   //database.ref().set('This is my data.')
+
+//   database.ref('test').set(
+//       'test01').then(() => 
+//       {console.log('Data saved')
+//     }).catch((e) =>
+//      {console.log('test001: ' + e)})
